f8977539a0d34185d496e544dac3f797e48d19b7
Viacheslav <maverik2047@yandex.ru>
2022-07-20 00:04:38 +0300

    second api test check

--- src/test/java/ReqresInTests.java
+++ src/test/java/ReqresInTests.java
@@ -1,10 +1,12 @@
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.core.Is.is;
 
 public class ReqresInTests {
     @Test
-    public void SingleUserNotFound(){
+    public void singleUserNotFound() {
         given()
                 .log().uri()
                 .log().body()
@@ -14,4 +16,19 @@ public class ReqresInTests {
                 .log().all()
                 .statusCode(404);
     }
+
+    @Test
+    public void getListUsers() {
+        given()
+                .log().uri()
+                .log().body()
+                .contentType(JSON)
+                .when()
+                .get("https://reqres.in/api/users?page=2")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("page", is(2));
+    }
+
 }
