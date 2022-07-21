5daf1748affe32e9f0453b985c0d5fa015f69e8c
Viacheslav <maverik2047@yandex.ru>
2022-07-21 23:10:31 +0300

    more tests added

--- src/test/java/ReqresInTests.java
+++ src/test/java/ReqresInTests.java
@@ -3,8 +3,12 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class ReqresInTests {
+    String url = "https://reqres.in";
+
     @Test
     public void singleUserNotFound() {
         given()
@@ -24,11 +28,87 @@ public class ReqresInTests {
                 .log().body()
                 .contentType(JSON)
                 .when()
-                .get("https://reqres.in/api/users?page=2")
+                .get(url + "/api/users?page=2")
                 .then()
                 .log().all()
                 .statusCode(200)
-                .body("page", is(2));
+                .body("total_pages", is(2));
+    }
+
+    @Test
+    public void createUser() {
+        given()
+                .log().uri()
+                .log().body()
+                .contentType(JSON)
+                .body("{ \"name\": \"morpheus\"}")
+                .when()
+                .post(url + "/api/users")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .body("name", is("morpheus"));
+
+    }
+
+    @Test
+    public void deleteUser() {
+        given()
+                .log().uri()
+                .log().body()
+                .contentType(JSON)
+                .when()
+                .delete(url + "/api/users/2")
+                .then()
+                .log().all()
+                .statusCode(204);
+    }
+
+    @Test
+    public void patchUser() {
+        given()
+                .log().uri()
+                .log().body()
+                .contentType(JSON)
+                .body("{ \"name\": \"morpheus\",\"job\": \"zion resident\"}")
+                .when()
+                .patch(url + "/api/users/2")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("job", is("zion resident"));
+    }
+
+    @Test
+    public void postLoginUnsuccessful() {
+        given()
+                .log().uri()
+                .log().body()
+                .contentType(JSON)
+                .body("{\"email\": \"peter@klaven\"}")
+                .when()
+                .post(url + "/api/login")
+                .then()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+
+    }
+
+    @Test
+    public void getSingleResourse() {
+        Integer expectedYear = 2001;
+        Integer actualYear = given()
+                .log().uri()
+                .when()
+                .get(url + "/api/unknown/2")
+                .then()
+                .log().body()
+                .statusCode(200)
+                .extract()
+                .path("data.year");
+        assertEquals(expectedYear, actualYear);
+
+
     }
 
 }
